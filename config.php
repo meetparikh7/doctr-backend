637cc4669f76b0bda4f907a078d9fb43843ac1f3
Meet Parikh <meetparikh7@gmail.com>
2015-06-28 14:09:26 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$KEY    = '@Mrs123';
+$dbhost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbuser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbpass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbname = getenv('OPENSHIFT_GEAR_NAME');
+?>
